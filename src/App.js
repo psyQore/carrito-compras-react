556efccb187f4796f83e2e0f7aabf4586aaeb75c
Juan Galarce <j6alarce@gmail.com>
2021-10-04 11:42:46 -0300

    Se crea el component title y navbar con su respectivo styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Component } from "react";
 import Products from "./components/Products";
 import Layout from "./components/Layout";
+import Title from "./components/Title";
+import Navbar from "./components/Navbar";
 
 import Tomate from "./assets/products-img/tomate.jpg";
 import Lechuga from "./assets/products-img/lechuga.jpg";
@@ -12,18 +14,21 @@ class App extends Component {
       { name: "Tomate", price: 500, img: Tomate },
       { name: "Arvejas", price: 2500, img: Arvejas },
       { name: "Lechuga", price: 600, img: Lechuga },
-      
     ],
   };
 
   render() {
     return (
-      <Layout>
-        <Products
-          addToCart={() => console.log("Click")}
-          products={this.state.products}
-        />
-      </Layout>
+      <>
+        <Navbar />
+        <Layout>
+          <Title />
+          <Products
+            addToCart={() => console.log("Click")}
+            products={this.state.products}
+          />
+        </Layout>
+      </>
     );
   }
 }
