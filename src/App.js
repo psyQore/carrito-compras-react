89f73f57468299718e5359b832267de6965d42c3
Juan Galarce <j6alarce@gmail.com>
2021-10-04 10:20:41 -0300

    Se crea el state y los products con sus datos e imagen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import Tomate from './assets/products-img/tomate.jpg'
+import Lechuga from './assets/products-img/lechuga.jpg'
+import Arvejas from './assets/products-img/arvejas.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    products: [
+      { name: "Tomate", price: 500, img: Tomate },
+      { name: "Arvejas", price: 2500, img: Arvejas },
+      { name: "Lechuga", price: 600, img: Lechuga },
+    ],
+  };
+
+  render() {
+    return <p>Hello World</p>;
+  }
 }
 
 export default App;
